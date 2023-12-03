3ec0c5baad17af300e35d0952e070f9992d1464b
Chae0510 <tia510@gachon.ac.kr>
2023-12-03 13:09:55 +0900

    FEAT: "firebase-profile image 구현" FIX: "nickname이 UI에서 확인 안되는 오류 수정"

--- lib/loginSuccess.dart
+++ lib/loginSuccess.dart
@@ -1,13 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'login.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:fortest/main.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 import 'alarmTap.dart'; // alarmTap.dart
 import 'categoryTap.dart'; // categoryTap.dart 파일
 import 'join.dart';
 import 'searchTap.dart';
+
+
+
 
 void goToAnotherPage(BuildContext context, String pageName){
   // 버튼에 따라 그에 해당하는 파일로 이동
@@ -47,15 +53,12 @@ class LoginSuccessScreen extends StatefulWidget {
 
 class UserData {
   String name;
-
-  String password;
   String email;
   String nickname;
-  String profileImgUrl;
+  String? profileImgUrl;
 
   UserData(
       {required this.name,
-        required this.password,
         required this.email,
         required this.nickname,
         required this.profileImgUrl,
@@ -66,21 +69,75 @@ class UserData {
 class _LoginSuccessScreenState extends State<LoginSuccessScreen> {
   UserData userData =
   UserData(name: '',
-      password: '',
       email: '',
       nickname: '',
       profileImgUrl: '');
 
   final picker = ImagePicker();
 
+  @override
+  void initState() {
+    super.initState();
+    loadUserData();
+  }
+
+  Future<void> loadUserData() async {
+    User? currentUser = FirebaseAuth.instance.currentUser;
+    print(currentUser);
+    if (currentUser != null) {
+      DocumentSnapshot userDataSnapshot = await FirebaseFirestore.instance.collection('users').doc(currentUser.uid).get();
+      print("Loaded user data: ${userDataSnapshot.data()}");
+      setState(() {
+        userData = UserData(
+          name: userDataSnapshot['username'],
+          email: currentUser.email ?? '',
+          nickname: userDataSnapshot['nickname'],
+          profileImgUrl: userDataSnapshot['profileImgUrl'],
+        );
+      });
+    }
+  }
   Future<void> _pickImageFromGallery() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
-    setState(() {
+
       if (pickedFile != null) {
-        userData.profileImgUrl = pickedFile.path;
+        File imageFile = File(pickedFile.path);
+
+        // Firebase Storage에 업로드
+        try {
+          // 사용자 ID 가져오기
+          String userId = FirebaseAuth.instance.currentUser!.uid;
+          firebase_storage.Reference ref = firebase_storage.FirebaseStorage
+              .instance
+              .ref('userProfileImages/$userId');
+
+          // 파일 업로드
+          firebase_storage.UploadTask uploadTask = ref.putFile(imageFile);
+
+          // 업로드 완료 대기
+          await uploadTask.whenComplete(() async {
+            // 업로드된 이미지의 URL 가져오기
+            String downloadURL = await ref.getDownloadURL();
+
+            // Firestore에 이미지 URL 업데이트
+            await FirebaseFirestore.instance.collection('users')
+                .doc(userId)
+                .update({
+              'profileImgUrl': downloadURL,
+            });
+
+            // 상태 업데이트
+            // 앱 상태 업데이트
+            setState(() {
+              userData.profileImgUrl = downloadURL;
+            });
+          });
+        } catch (e) {
+          // 오류 처리
+          print(e);
+        }
       }
-    });
   }
 
 
@@ -97,13 +154,12 @@ class _LoginSuccessScreenState extends State<LoginSuccessScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // 프로필 사진 위젯
-            userData.profileImgUrl != null
-                ? CircleAvatar(
-              radius: 50,
-              backgroundImage: FileImage(File(userData.profileImgUrl)),
-            )
-                : Container(), // 프로필 사진이 없으면 빈 컨테이너
+          CircleAvatar(
+          radius: 50,
+          backgroundImage: (userData.profileImgUrl != null && userData.profileImgUrl!.isNotEmpty)
+              ? NetworkImage(userData.profileImgUrl!)
+              : AssetImage('assets/image/default_image.png') as ImageProvider<Object>,
+          ),
             SizedBox(height: 20.0),
             // 갤러리에서 사진 고르기 버튼
             ElevatedButton(
@@ -113,14 +169,14 @@ class _LoginSuccessScreenState extends State<LoginSuccessScreen> {
 
             SizedBox(height: 20.0),
 
-            // OOO님 환영합니다~~
             Text(
-              '${userData.name}(${userData.nickname}) 님 환영합니다!',
+              '${userData.nickname ?? "사용자"} 님 환영합니다!',
               style: TextStyle(
                 fontSize: 24.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
+
             SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: () {
